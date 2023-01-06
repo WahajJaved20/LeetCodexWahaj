c82a15e1918cbd4d7b2422a43adca838ffa6db32
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-01-06 15:40:54 +0500

    Update CalculateSpecialBonus.sql

--- SQL/1873./CalculateSpecialBonus.sql
+++ SQL/1873./CalculateSpecialBonus.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select employee_id, 
 if(employee_id % 2 = 0 OR name LIKE 'M%', 0, salary) as bonus 
-from Employees;
+from Employees
+order by employee_id;
