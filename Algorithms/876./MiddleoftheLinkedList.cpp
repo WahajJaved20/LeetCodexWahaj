4e6951b911cfeedc6ce667e165eb110d25a84045
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-01-07 21:22:48 +0500

    Create MiddleoftheLinkedList.cpp

---
+++ Algorithms/876./MiddleoftheLinkedList.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* curr=head,*next=head;
+        while(next != NULL && next->next!=NULL){
+            curr = curr->next;
+            next = next->next->next;
+        }
+        return curr;
+    }
+};
