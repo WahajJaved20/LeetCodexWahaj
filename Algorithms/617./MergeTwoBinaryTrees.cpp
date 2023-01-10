e2a6fb2b9fc2bd9ecd508523e27df33ab8bf7ce4
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-01-10 14:04:44 +0500

    Create MergeTwoBinaryTrees.cpp

---
+++ Algorithms/617./MergeTwoBinaryTrees.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recur(TreeNode* root1, TreeNode* root2){
+        if(!root1){
+            return root2;
+        }
+        if(!root2){
+            return root1;
+        }
+        root1->val+=root2->val;
+        root1->left = recur(root1->left,root2->left);
+        root1->right = recur(root1->right,root2->right);
+        return root1;
+    }
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        TreeNode* merged = recur(root1,root2);
+        return merged;
+    }
+};
